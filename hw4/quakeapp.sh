f14f7794eea6827c5ec7ac23d48c562113f8a77a
Thunder Shiviah <thunder@uoregon.edu>
2014-04-24 10:58:02 -0700

    assigned the correct columns to magnitude. Current settings are for seattle. Outputs to quakeseattle.txt.

--- hw4/quakeapp.sh
+++ hw4/quakeapp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #returns a list of earthquake data fitting given specifications
-
+#Can't figure out how to use variables in awk.
 
 
 #YEAR_START=1900
@@ -21,15 +21,15 @@
 #	$2<LAT_END  && 
 #	$3<LONG_START   && 
 #	$3<LONG_END   && 
-#	$4<MAG_START   && 
-#	$4<MAG_END   && 
+#	$5<MAG_START   && 
+#	$5<MAG_END   && 
 #' new1.txt
 
-awk '$1>1900 && $1<1990 &&
-	$2>-3   &&       
-       $2<3  && 
-       $3>-20   && 
-       $3<20   && 
-       $4>1   && 
-       $4<5' new1.txt > quakeresult.txt
+awk '$1>1900 && $1<2010 &&
+	$2>40   &&       
+       $2<50  && 
+       $3>-130   && 
+       $3<-120   && 
+       $5>0   && 
+       $5<7' new1.txt > quakeseattle.txt
 
